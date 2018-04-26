3cb9d56a40808c3e3f3dfd56493a11339916a4b5
gaoyanan <yanan.gao@bitubit.com>
2018-04-26 14:10:59 +0800

    1

---
+++ src/main/java/controller/SpringBootApplication.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package controller;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * @author Admin
+ *
+ */
+@org.springframework.boot.autoconfigure.SpringBootApplication
+public class SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApplication.class, args);
+	}
+}
---
+++ src/main/java/controller/TestController.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Admin
+ *
+ */
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+	@RequestMapping("/hi")
+	public String hi(){
+		return "hi";
+	}
+	
+}
